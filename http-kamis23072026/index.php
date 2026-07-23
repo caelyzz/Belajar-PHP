e11d5514307ebfce41b4636ad9c93ade0b4fffd9
Junioraja <jr.official331@gmail.com>
2026-07-23 08:31:52 +0700

    http_kamis1

---
+++ http-kamis23072026/index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo $_GET['password'];
+
+?>
+
+<form action="index.php" method="get">
+    <input type="text" name="nama">
+    <input type="text" name="password">
+    <input type="submit" name="submit">
+</form>
