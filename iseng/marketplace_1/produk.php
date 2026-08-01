1d8a631c359192d046ba9d98a940cfdcce9ba7a8
Junioraja <jr.official331@gmail.com>
2026-08-01 19:15:43 +0700

    halaman produk 1

--- iseng/marketplace_1/produk.php
+++ iseng/marketplace_1/produk.php
@@ -4,3 +4,26 @@ session_start();
 
 $data = file_get_contents("produk.json");
 $produk = json_decode($data, true);
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Marketplace Halal
+        </title>
+    </head>
+    <body>
+        <h1>Daftar Produk</h1>
+        <?php foreach ($produk as $p){ ?>
+            <div>
+                <img src="gambar/<?=$p['gambar'];   ?>" width="150">
+                <h3><?=$p['nama'];?></h3>
+                <P>Harga : Rp<?= number_format($p['harga']); ?></P>
+                <p>Stok : <?=$p['stok']; ?></p>
+            </div>
+            <hr>
+        <?php } ?>
+    </body>
+</html>
