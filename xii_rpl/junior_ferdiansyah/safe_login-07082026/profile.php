bb7b153318f8d450e988405305939e1a457753f4
Junioraja <jr.official331@gmail.com>
2026-08-07 14:06:19 +0700

    add profile page

---
+++ xii_rpl/junior_ferdiansyah/safe_login-07082026/profile.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once "config/database.php";
+require_once "helpers/auth.php";
+require_once "helpers/remmeber.php";
+
+autoLogin();
+requiredLogin();
+
+$query = $pdo->prepare("
+    SELECT * FROM users WHERE id = ?
+");
+
+$query->execute(["user_id"]);
+
+$user = $query->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Profile</title>
+    </head>
+    <body>
+        <h2>Profile</h2>
+        <?php
+        if(!empty($user["avatar"])){
+        ?>
+
+        <img src="assets/uploads/avatars/<?= htmlspecialchars($user["avatar"])?>"
+        width="150" >
+
+        <?php } else { ?>
+        <P>Not yet had photo profile</P>
+        <?php } ?>
+
+        <P>Username : <b><?= htmlspecialchars($user["username"])?></b></P>
+        <p>Email : <?= htmlspecialchars($user["email"])?></b></p>
+        <hr>
+
+        <h3>Upload Avatar</h3>
+        <form action="process/upload_avatar.php" method="post" enctype="multipart/formdata" required>
+            <input type="file" name="avatar" accept=".jpg,.png,.jpeg,.webp,.gif" required>
+            <br><br>
+            <button type="submit">Upload</button>
+        </form>
+
+        <a href="home.php">Back</a>
+    </body>
+</html>
