3a15233d0317ead059368c6a8085f6af1be87e9f
Junioraja <jr.official331@gmail.com>
2026-08-07 12:52:12 +0700

    add page login

--- xii_rpl/junior_ferdiansyah/safe_login-07082026/login.php
+++ xii_rpl/junior_ferdiansyah/safe_login-07082026/login.php
@@ -6,6 +6,30 @@
     </head>
     <body>
         <h2>Login</h2>
-        <form action="process/login.php"></form>
+        <form action="process/login.php" method="post">
+            <label>Email</label>
+            <br>
+            <input type="email" name="email" required>
+            <br><br>
+
+            <label>Password</label>
+            <br>
+            <input type="password" name="password" required>
+            <br><br>
+
+            <label>
+                <input type="checkbox" name="remember">
+                Remember me
+            </label>
+            <br><br>
+
+            <button type="submit">
+                Login
+            </button>
+
+            <a href="register.php">
+                Don't have an account?
+            </a>
+        </form>
     </body>
 </html>
