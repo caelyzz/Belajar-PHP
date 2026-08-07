15f4edbe74e82ec1fb6ad12938c235efdf2d75b2
Junioraja <jr.official331@gmail.com>
2026-08-07 13:43:45 +0700

    add more function and fix logout

--- xii_rpl/junior_ferdiansyah/safe_login-07082026/process/logout.php
+++ xii_rpl/junior_ferdiansyah/safe_login-07082026/process/logout.php
@@ -1,12 +1,33 @@
 <?php
 
 require_once "../helpers/auth.php";
+require_once "../helpers/auth.php";
 
 startSession();
+
+if(isset($_SESSION["user_id"])){
+    $query = $pdo->prepare("
+        UPDATE users
+        SET remember_selector = NULL, remember_token = NULL
+        WHERE id = ?
+    ");
+
+    $query->execute($_SESSION["user_id"]);
+}
+
 
 $_SESSION = [];
 
 session_destroy();
+
+setcookie(
+    "remember_me",
+    "",
+    [
+        "expires"=>time() - 3600,
+        "path" => "/"
+    ]
+);
 
 header("Location: ../login.php");
 exit;
