a99789d273eb967be1686d06daf5826bc870a5f3
Junioraja <jr.official331@gmail.com>
2026-08-07 13:39:31 +0700

    add function autologin

--- xii_rpl/junior_ferdiansyah/safe_login-07082026/index.php
+++ xii_rpl/junior_ferdiansyah/safe_login-07082026/index.php
@@ -1,5 +1,16 @@
 <?php
 
-require 'config/database.php';
+require_once "helpers/remember.php";
 
-echo "Database berhasil terhubung.";
+autoLogin();
+
+startSession();
+
+if(isset($_SESSION["user_id"])){
+    header("Location: home.php");
+} 
+else{
+    header("Location: login.php");
+}
+
+exit;
