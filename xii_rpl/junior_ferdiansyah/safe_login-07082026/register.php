780b6b7670ffcd0db7082d9c5088ae30c3bdbe80
Junioraja <jr.official331@gmail.com>
2026-08-07 09:44:14 +0700

    add register page

---
+++ xii_rpl/junior_ferdiansyah/safe_login-07082026/register.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Register</title>
+    </head>
+    <body>
+        <h2>Register</h2>
+        <form action="process/register.php" method="POST">
+            <label>Username</label></form><br>
+            <input type="text" name="username" required>
+            <br><br>
+
+            <label>Email</label><br>
+            <input type="email" name="email" required>
+            <br><br>
+
+            <label>Password</label>
+            <input type="password" name="confirm_password" required>
+            <br><br>
+
+            <button type="submit">Register</button>
+        </form>
+        <br>
+
+        <a href="login.php">
+            Already have account?
+        </a>
+    </body>
+</html>
