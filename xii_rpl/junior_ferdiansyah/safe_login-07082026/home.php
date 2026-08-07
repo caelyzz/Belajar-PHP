ae9923f3703128734fa584466ed5556c1d21d17d
Junioraja <jr.official331@gmail.com>
2026-08-07 13:12:15 +0700

    add home

--- xii_rpl/junior_ferdiansyah/safe_login-07082026/home.php
+++ xii_rpl/junior_ferdiansyah/safe_login-07082026/home.php
@@ -5,3 +5,23 @@ require_once 'helpers/auth.php';
 requiredLogin();
 startSession();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+    </head>
+    <body>
+        <h2>Home</h2>
+
+        <p>Welcome,
+            <b><?= htmlspecialchars($_SESSION["username"]) ?></b>
+        </p>
+        <p>
+            Email : <?= htmlspecialchars($_SESSION["email"])?>
+        </p>
+        <br>
+        <a href="process/logout.php">Logout</a>
+    </body>
+</html>
