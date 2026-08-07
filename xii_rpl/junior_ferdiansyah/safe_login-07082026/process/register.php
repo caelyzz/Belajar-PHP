b163d408d98223b56da81926150f4646b5818084
Junioraja <jr.official331@gmail.com>
2026-08-07 12:47:30 +0700

    add process register

---
+++ xii_rpl/junior_ferdiansyah/safe_login-07082026/process/register.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once "../config/database.php";
+
+$username = trim($_POST["username"]);
+$email = trim($_POST["email"]);
+$password = trim($_POST["password"]);
+$confirmPassword = trim($_POST["confirm_password"]);
+
+if (
+    empty($username) || empty($email) ||
+    empty($password) || empty($confirmPassword)
+){
+    die("All field must be filled");
+}
+
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    die("Email format not valid");
+}
+
+if (strlen($password)<8){
+    die("Minimal password character is 8");
+}
+
+$query = $pdo->prepare(
+    "SELECT id FROM users WHERE email = ?"
+);
+
+$query->execute([$email]);
+
+if ($query->fetch()){
+    die("Email has been registered");
+}
+
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+$query = $pdo->prepare("
+    INSERT INTO users(
+        username,
+        email,
+        password
+    )
+    VALUES(
+        ?, ?, ?
+    )
+");
+
+$query->execute([
+    $username,
+    $email,
+    $passwordHash
+]);
+
+header("Location: ../login.php");
+exit;
